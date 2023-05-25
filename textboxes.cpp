8cf3846e10658be43f914a0d70cfd0f2b935a2b9
adrian <adrian_lukaszyk@wp.pl>
2023-05-25 22:56:59 +0200

    initial commit

---
+++ textboxes.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "textboxes.hpp"
+
+
+sf::Font TextBoxes::set_font(const std::string &font = "PcSeniorRegular-OEnd.ttf") {
+    sf::Font f;
+    if(!f.loadFromFile(font))
+        std::cerr << "Blad w ladowaniu czcionki!" << std::endl;
+    return f;
+}
+
+void TextBoxes::set_boxes(int pos_x = 10, int pos_y = 10) {
+    auto initial_pos = sf::Vector2f(pos_x, pos_y);
+    int font_size = 8;
+
+    sf::Text mouse_pos;
+    sf::Text fps;
+
+    this->t_boxes.push_back(mouse_pos);
+    this->t_boxes.push_back(fps);
+
+    for(auto &it : this->t_boxes){
+        it.setFont(this->font);
+        it.setFillColor(sf::Color::Red);
+        it.setCharacterSize(8);
+        // it.setPosition(initial_pos);
+    }
+
+    // for()
+
+    this->t_boxes.at(0).setPosition(initial_pos);
+    initial_pos = sf::Vector2f(initial_pos.x + font_size + 2, initial_pos.y);
+    this->t_boxes.at(1).setPosition(initial_pos);
+
+}
--- textboxes.hpp
+++ textboxes.hpp
@@ -3,6 +3,29 @@
 
 #include <SFML/Graphics.hpp>
 
+class FPS;
 
+class TextBoxes : public sf::Drawable {
+public:
+    TextBoxes() :
+    font(std::move(set_font())) {
+        set_boxes();
+    };
+
+    sf::Font set_font(const std::string &f = "PcSeniorRegular-OEnd.ttf");
+    void set_boxes(int pos_x = 10, int pos_y = 10);
+
+    friend class FPS;
+
+private:
+    sf::Font font;
+    std::vector<sf::Text> t_boxes;
+    /* sf::Text mouse_pos_text_box;
+    sf::Text fps_text_box; */
+};
+
+class FPS {
+
+};
 
 #endif // _TEXTBOXES_HPP_
